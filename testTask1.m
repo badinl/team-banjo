25f5a92726cea8089c6b8447fb5939cafdf3a69e
Luca Badin <lucabad97@gmail.com>
2020-11-27 22:53:36 +0100

    Task 1 test corretto

--- testTask1.m
+++ testTask1.m
@@ -7,8 +7,8 @@ x = false(1,7);
 % we shall store stats in two maps
 % can't put logical as key type...
 % convert and store as int32 unless there's a better way?
-ystats = zeros(65);
-zstats = zeros(113);
+ystats = zeros(65,1);
+zstats = zeros(113,1);
 
 % ystats = containers.Map('KeyType','int32','ValueType','int32');
 % zstats = containers.Map('KeyType','int32','ValueType','int32');
@@ -16,11 +16,8 @@ zstats = zeros(113);
 for i = 1:10000
     [y,z] = wiretap(x);
     y = convertBString(y) + 1;
-    ystats(y) = ystats(y) + 1;
+    ystats(y,1) = ystats(y,1) + 1;
     z = convertBString(z) + 1;
-    zstats(z) = ystats(z) + 1;
+    zstats(z,1) = zstats(z,1) + 1;
 end
-
-ystats
-zstats
 end
