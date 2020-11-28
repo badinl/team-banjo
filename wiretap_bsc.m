9a1a5d893b731c642cc154b079f04dbb824fb3d6
Luca Badin <lucabad97@gmail.com>
2020-11-28 16:55:20 +0100

    Task 5

---
+++ wiretap_bsc.m
@@ -0,0 +1,16 @@
+function [y,z] = wiretap_bsc(x,e,d)
+% Task 5: Implement the "true" wiretap BSC channel
+y = x;
+z = x;
+
+for i=1:size(x,2)
+    r = rand();
+    if rand() < e
+        y(i) = not(x(i));
+    end
+    if rand() < d
+        z(i) = not(x(i));
+    end
+end
+end
+
